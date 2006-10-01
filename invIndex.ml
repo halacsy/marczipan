e28a2d4b70cebfcc577591395f93f3c6ba17904d
hp <hp@2fd4087b-5035-0410-80e8-bea57dacc43b>
2006-10-01 16:19:51 +0000

    posting file pointere is 64 bites lett
    git-svn-id: file:///local/svn/marcipan/trunk@78 2fd4087b-5035-0410-80e8-bea57dacc43b

--- invIndex.ml
+++ invIndex.ml
@@ -5,7 +5,7 @@ type writer = {doclist_oc : out_channel;
 			   lexicon_oc : out_channel
 }
 
-type term_record = (int * int * int) 
+type term_record = (int * int * Int64.t) 
 
 type reader = {doclist_ic : in_channel;
 			   lexicon      : (term_record) Lex.t;
@@ -21,13 +21,13 @@ let open_writer index_dir =
 
 let write_term_entry w term terminfo =
 	flush w.doclist_oc;
-	let pos = pos_out w.doclist_oc in
+	let pos = LargeFile.pos_out w.doclist_oc in
 	let df = DocList.Collector.df terminfo in
 	let tf = DocList.Collector.tf terminfo in
 	Io.output_string w.lexicon_oc term;
 	output_binary_int w.lexicon_oc df;
 	output_binary_int w.lexicon_oc tf;
-	output_binary_int w.lexicon_oc pos	;
+	Io.output_vint64 w.lexicon_oc pos	;
 	DocList.write w.doclist_oc (DocList.Collector.doclist terminfo)
 ;;
 
@@ -50,7 +50,7 @@ let open_reader index_dir =
 		let term = Io.input_string lic in
 		let df   = input_binary_int lic in
 		let tf   = input_binary_int lic in
-		let pos  = input_binary_int lic in
+		let pos  = Io.input_vint64 lic in
 		Lex.update reader.lexicon (df, tf, pos) term (fun x -> x);
 		reader.tokens <- reader.tokens + tf;
 		loop ()
@@ -71,7 +71,7 @@ let term_info reader term =
 	let (df, tf, pos) = Lex.find reader.lexicon term in
 	let open_stream () =
 		Timem.start reader.stopper "loading postings";
-		seek_in reader.doclist_ic  pos;
+		LargeFile.seek_in reader.doclist_ic  pos;
 		let doclist = DocList.read reader.doclist_ic  df in
 		Timem.finish reader.stopper ;
     	DocList.open_stream doclist
@@ -82,7 +82,7 @@ let pretty_print reader =
 	let aux term (df, tf, pos) =
 		Printf.printf "%s -> " term;
 	    Printf.printf "df = %d tf = %d\n " df tf;
-		seek_in reader.doclist_ic  pos;
+		LargeFile.seek_in reader.doclist_ic  pos;
 		let doclist = DocList.read reader.doclist_ic  df in
 		DocList.pretty_print doclist ; 
 	in
